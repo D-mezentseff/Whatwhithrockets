f46289a845be8c94e82533d43235b923f83a03c5
d_mezentseff <danseva4@gmail.com>
2021-04-19 18:07:21 +0300

    docs: lection 2021.04.19

--- README.md
+++ README.md
@@ -304,3 +304,137 @@ const tempStat = temperatures.reduce(
     }, { positive : 0, negative : 0}
 )
 ```
+
+
+#### Массив - объект
+```js
+const arr = [1, 2]
+// arr[100] = 34
+arr[-33] = 'Alloha'
+arr["Турция"] = 'Отпуск'
+arr["55.3"] = "Test"
+arr[arr] = "Array"
+
+console.log(arr)
+```
+
+
+### reduceRight как обычный, только арабский
+Идёт с _Lenght - 1_ индекса по _0_-ой
+
+
+### reverse
+разворачивает массив, мутирующий метод
+
+```js 
+const arr = [1, 2]
+arr.a = 3
+arr.b = 4
+arr.reverse()
+
+console.log(arr)
+```
+### fill 
+Заполняет массив чем-то
+
+```js
+const arr = [1, 2]
+arr.fill(777)
+console.log(arr)
+
+const arr2 = new Array(10)
+arr2.fill()
+console.log(arr2)
+```
+### includes 
+```js
+const arr = [1, 2]
+
+console.log(arr.includes(2))
+```
+
+### flat
+
+```js 
+const arr = [1, 2, [3, 2, [3, 5]]]
+
+console.log(arr.flat(Number.MAX_SAFE_INTEGER))
+```
+### slice, splice 
+
+__splice:__
+ - Обрезать
+ - Вырезать
+ - Заменять
+ - Добавлять (в начало, конец, середину)
+```js
+const got = ['Dayneris', 'John Snow', 'Dragon 1', 
+'Dead Dragon 2', 'Serceya Lanister']
+
+//slice - не мутирует
+console.log(got.slice(1, 3))
+console.log(got.slice())
+console.log(got.slice(-2))
+console.log(got.slice(1, -2))
+
+//splice - обрезать с 1 и до конца
+const tempGot = got.splice(1)
+console.log(got)
+console.log(tempGot)
+
+//splice - вырезка конкретного элемента из массива
+const tempGot = got.splice(1, 1)
+console.log(got)
+console.log(tempGot)
+
+//splice - вырезка конкретного элемента из массива
+const tempGot = got.splice(1, 1, 'Иван Снег', 'John Doe')
+console.log(got)
+console.log(tempGot)
+```
+
+### concat
+Добавляет в конец нового массива элементы принимая их как:
+ - перечеь аргументов ```js const got2 = got.concat(1, 2, 3) ```
+ - перечень массивов ```js const got2 = got.concat([1, 2], [3, 4]) ```
+ 
+ ```js
+const got2 = got.concat(1, 2, 3)
+console.log(got2)
+console.log(got)
+
+const got2 = [].concat(1, 2, 3, got)
+console.log(got2)
+console.log(got)
+ ```
+
+ ### from
+ Создаёт массив из чего-то похожего на массив из:
+  - строк
+
+
+  ```js
+    const got2 = Array.from(got)
+    got2.a = 34343
+    console.log(got2)
+    console.log(got)
+
+    const dragon = {
+        weight : 2000,
+        wings : 2,
+        isFire : true,
+        wingsWidth : 30,
+        name : 'Drakon',
+        owner : "Dayaneris"  
+        
+    }
+    const got2 = Array.from(dragon)
+    // console.log(got2)
+    function shownGot(el) {
+        console.log(...arguments)
+        el.forEach(element => console.log(element))
+    }
+    shownGot(got)
+    console.log(Array.from([1, 2, 3], x => x + x));
+    // expected output: Array [2, 4, 6]
+  ```
--- lecture/app/lesson.js
+++ lecture/app/lesson.js
@@ -1,4 +1,4 @@
-const arr = [11, 72, 3, 9, 0]
+// const arr = [11, 72, 3, 9, 0]
 
 // arr[33] = {}
 // arr.length = 123
@@ -38,37 +38,79 @@ const arr = [11, 72, 3, 9, 0]
 // arr.sort(sortLikeNums)
 // console.log(arr)
 
-const temperatures = [13, 6, 7, 11, 2, 0, 15, -3, 17]
+// const temperatures = [13, 6, 7, 11, 2, 0, 15, -3, 17]
 
-const tempsF = temperatures.map((el) => el * 1.8 +32)
+// const tempsF = temperatures.map((el) => el * 1.8 +32)
 
-const tempsWarm = temperatures.filter(el => el>= 15)
+// const tempsWarm = temperatures.filter(el => el>= 15)
 
-const coldTemp = temperatures.find(el => el< 0)
+// const coldTemp = temperatures.find(el => el< 0)
 
-const coldTempId = temperatures.findIndex(el => el< 0)
+// const coldTempId = temperatures.findIndex(el => el< 0)
 
 
-// console.log(temperatures)
-// console.log(coldTemp)
-// console.log(coldTempId)
+// // console.log(temperatures)
+// // console.log(coldTemp)
+// // console.log(coldTempId)
 
-const cities = ['London', 'Tokyo', 'Paris', 'New York', 'Pavlograd']
+// const cities = ['London', 'Tokyo', 'Paris', 'New York', 'Pavlograd']
 
-cities.reduce((acc, el, i, arr) => {
-    // console.log(acc, el, i)
-    return el
-})
+// cities.reduce((acc, el, i, arr) => {
+//     // console.log(acc, el, i)
+//     return el
+// })
 
-const tempHighest = temperatures.reduce((acc, el) => acc > el ? acc : el)
+// const tempHighest = temperatures.reduce((acc, el) => acc > el ? acc : el)
 
-// console.log(tempHighest)
+// // console.log(tempHighest)
 
-const tempStat = temperatures.reduce(
-    (acc, el) => {
-        el < 0 ? acc.negative++ : acc.positive++; 
-        // console.log(acc, el)
-        return acc
-    }, { positive : 0, negative : 0}
-)
-console.log(tempStat)
+// const tempStat = temperatures.reduce(
+//     (acc, el) => {
+//         el < 0 ? acc.negative++ : acc.positive++; 
+//         // console.log(acc, el)
+//         return acc
+//     }, { positive : 0, negative : 0}
+// )
+// console.log(tempStat)
+
+
+
+const got = ['Dayneris', 'John Snow', 'Dragon 1', 
+'Dead Dragon 2', 'Serceya Lanister']
+
+//splice - вырезка конкретного элемента из массива
+// const tempGot = got.splice(1, 1, 'Иван Снег', 'John Doe')
+// console.log(got)
+// console.log(tempGot)
+
+// const got2 = [].concat(1, 2, 3, got)
+// console.log(got2)
+// console.log(got)
+console.log(Array.from('Hello'))
+
+// const got2 = Array.from(got)
+// got2.a = 34343
+// console.log(got2)
+// console.log(got)
+
+const dragon = {
+    weight : 2000,
+    wings : 2,
+    isFire : true,
+    wingsWidth : 30,
+    name : 'Drakon',
+    owner : "Dayaneris"  
+      
+}
+const got2 = Array.from(dragon)
+// console.log(got2)
+function shownGot(el) {
+    console.log(...arguments)
+    el.forEach(element => console.log(element))
+}
+shownGot(got)
+
+
+console.log(Array.from([1, 2, 3], x => x + x));
+// expected output: Array [2, 4, 6]
+
